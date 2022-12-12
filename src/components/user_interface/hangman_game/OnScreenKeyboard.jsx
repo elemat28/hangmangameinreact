a4d60fd3764598b36764b5eb421d9bdf4433c7c1
Mateusz Markowski <elemat28@gmail.com>
2022-12-12 15:27:36 +0000

    Crated a dummy keyboard layout

--- src/components/user_interface/hangman_game/OnScreenKeyboard.jsx
+++ src/components/user_interface/hangman_game/OnScreenKeyboard.jsx
@@ -1,11 +1,25 @@
-import React from 'react'
-import Button from '@mui/material/Button';
+import React from "react";
+import Button from "@mui/material/Button";
 function OnScreenKeyboard() {
   return (
-    <div className='OnScreenKeyboard'>
-        <Button variant="contained">Hello World</Button>
+    <div className="on-screen-keyboard">
+      <div className="keyboard-row">
+        <Button variant="contained">Q</Button>
+        <Button variant="contained">W</Button>
+        <Button variant="contained">E</Button>
+      </div>
+      <div className="keyboard-row">
+        <Button variant="contained">A</Button>
+        <Button variant="contained">S</Button>
+        <Button variant="contained">D</Button>
+      </div>
+      <div className="keyboard-row">
+        <Button variant="contained">Z</Button>
+        <Button variant="contained">X</Button>
+        <Button variant="contained">C</Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default OnScreenKeyboard
+export default OnScreenKeyboard;
