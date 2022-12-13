462bb6467c2870929f3995bd40bf8b51f59c9624
Mateusz Markowski <elemat28@gmail.com>
2022-12-13 06:50:52 +0000

    Stateful button toggles state when pressed

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,39 @@
+import React from "react";
+import { ThemeProvider } from "@emotion/react";
 import "./App.css";
-import ThemeSelector from "./components/user_interface/general/ThemeSelector";
-
+import getUserPrefferedTheme, {
+  ResolveThemeToUse,
+} from "./components/user_interface/general/ThemeSelector";
+import CssBaseline from "@mui/material/CssBaseline";
 import Hangman from "./components/hangman_game/Hangman";
 import OnScreenKeyboard from "./components/user_interface/hangman_game/OnScreenKeyboard";
 import HangmanImage from "./components/user_interface/hangman_game/HangmanImage";
 import Word from "./components/user_interface/hangman_game/Word";
-
+import { useState } from "react";
 import RandomWordGenerator from "./components/data_fetch/RandomWordGenerator";
 import FetchDefinition from "./components/data_fetch/FetchDefinition";
+import ThemeSelector from "./components/user_interface/general/ThemeSelector";
 
 function App() {
+  let userTheme = ResolveThemeToUse();
   return (
     <div className="App">
-      <header>
-        <p>Hangman Game App</p>
-        <ThemeSelector />
-      </header>
-      <div className="HangmanGame">
-        <Word />
-        <HangmanImage />
-        <div className="keyboard-div">
-          <OnScreenKeyboard />
-        </div>
-      </div>
+      <ThemeProvider theme={userTheme}>
+        <CssBaseline />
+        <header>
+          <p>Hangman Game App</p>
+          <ThemeSelector />
+        </header>
+        <main>
+          <div className="HangmanGame">
+            <Word />
+            <HangmanImage />
+            <div className="keyboard-div">
+              <OnScreenKeyboard />
+            </div>
+          </div>
+        </main>
+      </ThemeProvider>
     </div>
   );
 }
--- src/components/user_interface/general/ThemeSelector.jsx
+++ src/components/user_interface/general/ThemeSelector.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React from "react";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Stack from "@mui/material/Stack";
+import { useState } from "react";
 
+import { themes } from "../themes";
+import SettingsBrightnessTwoToneIcon from "@mui/icons-material/SettingsBrightnessTwoTone";
+import Checkbox from "@mui/material/Checkbox";
+import FormGroup from "@mui/material/FormGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import FormControl from "@mui/material/FormControl";
+import FormLabel from "@mui/material/FormLabel";
 function ThemeSelector() {
+  const label = { inputProps: { label: "Checkbox demo" } };
+  const [checked, setChecked] = React.useState(false);
+
+  const handleChange = (event) => {
+    setChecked(!checked);
+    console.debug(event);
+  };
   return (
-    <div className='ThemeSelector'></div>
-  )
+    <div>
+      <FormControlLabel
+        value="top"
+        control={
+          <Checkbox
+            checked={checked}
+            onChange={handleChange}
+            inputProps={{ "aria-label": "controlled" }}
+            {...label}
+            icon={<SettingsBrightnessTwoToneIcon />}
+            checkedIcon={<SettingsBrightnessTwoToneIcon />}
+            id="theme_toggler"
+          />
+        }
+        label="Toggle dark mode"
+        labelPlacement="bottom"
+      />
+    </div>
+  );
 }
 
-export default ThemeSelector
+export function ResolveThemeToUse() {
+  //todo: check is any value stored for preference
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  if (prefersDarkMode) {
+    return themes.dark;
+  } else {
+    return themes.light;
+  }
+}
+
+export default ThemeSelector;
